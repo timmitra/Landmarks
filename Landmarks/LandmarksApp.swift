bf0253ec4d11cf27b8f6bcb82b00283e0dc77c48
Tim Mitra <twitter@it-guy.com>
2021-02-20 14:09:33 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tim Mitra on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
